6427bf4c90e6ab3643c488d0f44a2f5fe1d57cc0
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-09-08 10:46:15 -0300

    Desafio concluído!!

---
+++ src/MainConversor.java
@@ -0,0 +1,7 @@
+public class MainConversor {
+    public static void main (String[] args){
+        System.out.println("Iniciando a aplicação...");
+        conversor_de_moeda conversor = new conversor_de_moeda();
+        conversor.SolicitarDados();
+    }
+}
---
+++ src/conversor_de_moeda.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class conversor_de_moeda {
+    public String moedaOrigem;
+    public String moedaDestino;
+    public double valorCotacaoOrigem;
+    public double valorCotacaoDestino;
+
+    public void SolicitarDados(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Moeda de origem: ");
+        moedaOrigem = scanner.nextLine();
+
+        System.out.println("Moeda de destino: ");
+        moedaDestino = scanner.nextLine();
+
+        System.out.println("Qual o valor de 1 " + moedaOrigem + " em " + moedaDestino + "?");
+        valorCotacaoOrigem = scanner.nextDouble();
+
+        efetuarConversao();
+    }
+
+    private void efetuarConversao(){
+        valorCotacaoDestino = 1 / valorCotacaoOrigem;
+        exibirResultado();
+    }
+
+    private void exibirResultado(){
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("R E S U L T A D O S");
+        System.out.println("Moeda de origem " + moedaOrigem);
+        System.out.println("Moeda de destino " + moedaDestino);
+        String valorCotacaoDestinoComDoisDecimais = String.format("%.2f", valorCotacaoDestino);
+        System.out.println("1 + " + moedaOrigem + " = " + valorCotacaoDestinoComDoisDecimais + "" + moedaDestino);
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxx");
+    }
+}
